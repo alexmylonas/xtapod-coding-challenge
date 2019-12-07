f9d498c8fb7d6a06d2ca33a196dd7a6016499152
Alex Mylonas <alex.a.mylonas@gmail.com>
2019-12-07 19:45:15 +0200

    Changes in PageComponent

--- client/scripts/ui/PageComponent.js
+++ client/scripts/ui/PageComponent.js
@@ -5,14 +5,17 @@ function PageComponent(bind,args) {
 
   // Usage: fs.mix(parent, ComponentPath, Arguments)
   // fs.mix is the most basic way to nest components
-  fs.mix(this,'ui/ExampleA');
-  fs.mix(this,'ui/ExampleB');
-
+// s.mix(this,'ui/ExampleA');
+  fs.mix(this,'ui/TopBar');
+  fs.mix(this, 'ui/Sidebar');
+  fs.mix(this, 'ui/Main');
+  fs.mix(this, 'ui/MainContent');
+  // fs.mix(this,'ui/ExampleB');
   // Usage: fs.mixAppend(domElementReference, ComponentPath, Arguments)
   // fs.mixAppend is similar to fs.mix except you can add component to a dom element
-  fs.mixAppend($('.appended-elements',this.dom),'ui/AppendedExample', {text: 'some text'});
-  fs.mixAppend($('.appended-elements',this.dom),'ui/AppendedExample');
-  fs.mixAppend($('.appended-elements',this.dom),'ui/AppendedExample');
+  // fs.mixAppend($('.appended-elements',this.dom),'ui/AppendedExample', {text: 'some text'});
+  // fs.mixAppend($('.appended-elements',this.dom),'ui/AppendedExample');
+  // fs.mixAppend($('.appended-elements',this.dom),'ui/AppendedExample');
 
   // Mixlists are essentially the same as mixAppend but they're more managed (allows for sorting, deleting, etc)
   this.list = fs.mixList(this,'.mix-list');
